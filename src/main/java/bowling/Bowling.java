e48624fccc6dec2c06dc3b893c81df30386bc94a
rimuli123 <yellowdoge1996@gmail.com>
2020-10-30 19:21:24 +0900

    重写

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -7,10 +7,12 @@ import java.util.List;
  * @author yellowdoge1996
  */
 public class Bowling {
-    List<Integer> rolls = new ArrayList<Integer>();
+    int[] rolls = new int[21];
+    int rollsIndex = 0;
 
     public void roll(int n) {
-        rolls.add(n);
+        this.rolls[rollsIndex] = n;
+        this.rollsIndex++;
     }
 
     /**
@@ -22,13 +24,13 @@ public class Bowling {
 
         int score = 0;
 
-        for (int i = 0, index = 0, length = 10; i < length; i++,index++){
-            if (rolls.get(index) + rolls.get(index + 1) > 10){
-                score += 10 + rolls.get(index + 1) + rolls.get(index + 1);
-            }else if (rolls.get(index) + rolls.get(index + 1) == 10){
-                score += 10 + rolls.get(++index  + 1);
-            }else {
-                score += rolls.get(index) + rolls.get( ++index  );
+        for (int i = 0, index = 0, length = 10; i < length; i++, index++) {
+            if (this.rolls[index] + this.rolls[index + 1] > 10) {
+                score += 10 + this.rolls[index + 1] + this.rolls[index + 1];
+            } else if (this.rolls[index] + this.rolls[index + 1] == 10) {
+                score += 10 + this.rolls[++index + 1];
+            } else {
+                score += this.rolls[index] + this.rolls[++index];
             }
         }
         return score;
