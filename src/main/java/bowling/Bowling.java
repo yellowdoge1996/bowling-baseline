5b529791688ca9f67036bd38ac4c187dbb556896
rimuli123 <yellowdoge1996@gmail.com>
2020-10-30 19:03:24 +0900

    重写

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -19,17 +19,10 @@ public class Bowling {
      * @return 得分
      */
     public int getScore() {
-//        if (score != 0) {
-//            score = 0;
-//        }
 
         int score = 0;
-        int index = 0;
-        int length = 10;
-//        int round = 1;
-//        int bollNumber = 1;
 
-        for (int i = 0; i < length; i++,index++){
+        for (int i = 0, index = 0, length = 10; i < length; i++,index++){
             if (this.rolls[index] + this.rolls[index + 1] > 10){
                 score += 10 + this.rolls[index + 1] + this.rolls[index + 1];
             }else if (this.rolls[index] + this.rolls[index + 1] == 10){
@@ -37,36 +30,7 @@ public class Bowling {
             }else {
                 score += this.rolls[index] + this.rolls[ ++index  ];
             }
-//            index = index + (((this.rolls[index] + this.rolls[index + 1]) > 10) ? 1 : 2);
-//            System.out.println(score);
         }
-
-//        for (int i = 0; i < this.rollsIndex; i++) {
-//            if (round > 10) {
-//                break;
-//            }
-//            score += this.rolls[i];
-//            if (bollNumber == 1) {
-//                if (this.rolls[i] == 10) {
-//                    //Strike
-//                    score = score + this.rolls[i + 1] + this.rolls[i + 2];
-//                } else {
-//                    bollNumber++;
-//                    continue;
-//                }
-//            } else {
-//                if (this.rolls[i] + this.rolls[i - 1] == 10) {
-//                    //spare
-//                    score = score + this.rolls[i + 1];
-//                }
-////                else {
-//                    //other
-////                    score = score + this.rolls[i] + this.rolls[i - 1];
-////                }
-//                bollNumber = 1;
-//            }
-//            round++;
-//        }
         return score;
     }
 }
