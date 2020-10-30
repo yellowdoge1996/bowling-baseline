4fa76c8b32088cf6640d0eaae06c85975c4af9a4
rimuli123 <yellowdoge1996@gmail.com>
2020-10-30 18:22:24 +0900

    重写

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -7,7 +7,6 @@ public class Bowling {
     int score;
     int[] rolls = new int[21];
     int rollsIndex = 0;
-    private static final int ALL_DOWN = 10;
 
     public void roll(int n) {
         this.rolls[rollsIndex] = n;
@@ -24,42 +23,46 @@ public class Bowling {
             this.score = 0;
         }
 
-        int round = 1;
-        int bollNumber = 1;
-//        int[] game = new int[10];
-//        int index = 0;
+        int[] game = new int[10];
+        int index = 0;
 
-//        for (int i = 0; i < game.length; i++){
-//            if (this.rolls[index] == ALL_DOWN){
-//                game[i] = ALL_DOWN + this.rolls[i + 1] + this.rolls[i + 2];
-//            }
-//        }
-        for (int i = 0; i < this.rollsIndex; i++) {
-            if (round > 10) {
-                break;
+        for (int i = 0; i < game.length; i++){
+            if (this.rolls[index] + this.rolls[index + 1] > 10){
+                this.score += 10 + this.rolls[index + 1] + this.rolls[index + 2];
+            }else if (this.rolls[index] + this.rolls[index + 1] == 10){
+                this.score += 10 + this.rolls[index + 1];
+            }else {
+                this.score += this.rolls[index] + this.rolls[index + 1];
             }
-            this.score += this.rolls[i];
-            if (bollNumber == 1) {
-                if (this.rolls[i] == 10) {
-                    //Strike
-                    this.score = this.score + this.rolls[i + 1] + this.rolls[i + 2];
-                    round++;
-                } else {
-                    bollNumber++;
-                }
-            } else {
-                if (this.rolls[i] + this.rolls[i - 1] == 10) {
-                    //spare
-                    this.score = this.score + this.rolls[i + 1];
-                }
-//                else {
-                    //other
-//                    this.score = this.score + this.rolls[i] + this.rolls[i - 1];
-//                }
-                round++;
-                bollNumber = 1;
-            }
+            index = index + (((this.rolls[index] + this.rolls[index + 1]) > 10) ? 1 : 2);
         }
+
+//        for (int i = 0; i < this.rollsIndex; i++) {
+//            if (round > 10) {
+//                break;
+//            }
+//            this.score += this.rolls[i];
+//            if (bollNumber == 1) {
+//                if (this.rolls[i] == 10) {
+//                    //Strike
+//                    this.score = this.score + this.rolls[i + 1] + this.rolls[i + 2];
+//                } else {
+//                    bollNumber++;
+//                    break;
+//                }
+//            } else {
+//                if (this.rolls[i] + this.rolls[i - 1] == 10) {
+//                    //spare
+//                    this.score = this.score + this.rolls[i + 1];
+//                }
+////                else {
+//                    //other
+////                    this.score = this.score + this.rolls[i] + this.rolls[i - 1];
+////                }
+//                bollNumber = 1;
+//            }
+//            round++;
+//        }
         return this.score;
     }
 }
